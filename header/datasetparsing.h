c15f6b68536412e7f61f6975d17729a46de2d487
Rushilkothur <143026236+Rushilkothur@users.noreply.github.com>
2024-04-18 16:28:44 -0400

    Update datasetparsing.h
    change parse_entry function to return vector of all tweets

--- header/datasetparsing.h
+++ header/datasetparsing.h
@@ -1,13 +1,44 @@
 #pragma once
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
 #include "Tweet.h"
 
-Tweet parse_entry()
+std::vector<Tweet> parse_entry()
 {
-    std::string tweet;//load values from csv in here
-    int emotiontag;//this is the second integer from the csv file
-    //parsing here
+    std::string filename{"text.csv"};
+    std::ifstream input{filename};
 
-    //after you've loaded values from the file into tweet and emotion tag, then attach them to a tweet class object 
-    Tweet obj(tweet, emotiontag);
-    return obj;
+    if (!input.is_open()) {
+        std::cerr << "Couldn't read file: " << filename << "\n";
+        return 1; 
+    }
+
+    std::vector<std::vector<std::string>> csvRows;
+
+    for (std::string line; std::getline(input, line);) {
+        std::istringstream ss(std::move(line));
+        std::vector<std::string> row;
+        if (!csvRows.empty()) {
+            // We expect each row to be as big as the first row
+            row.reserve(csvRows.front().size());
+        }
+        // std::getline can split on other characters, here we use ','
+        for (std::string value; std::getline(ss, value, ',');) {
+        row.push_back(std::move(value));
+        }
+        csvRows.push_back(std::move(row));
+    }
+
+    std::vector<Tweet> tweets;
+    for (int i = 1; i < csvRows.size(); i++) {
+        std::string tweet = csvRows[i][1];
+        int emotiontag = stoi(csvRows[i][2];
+        Tweet obj(tweet, emotiontag);
+        tweets.push_back(obj);
+    }
+    return tweets;
 }
